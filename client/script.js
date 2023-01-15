1c37fbdd3ace3a930e0f39ab2e69be00055c7e6f
titowing <sirskibox>
2023-01-14 18:22:22 -0600

    modified package.lock & json script.js

--- client/script.js
+++ client/script.js
@@ -1,34 +1,34 @@
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector('form')
+const chatContainer = document.querySelector('#chat_container')
 
-let loadInterval;
+let loadInterval
 
 function loader(element) {
-    element.textContent = '';
+    element.textContent = ''
 
     loadInterval = setInterval(() => {
         // Update the text content of the loading indicator
         element.textContent += '.';
 
         // If the loading indicator has reached three dots, reset it
-        if (element.textContent && element.textContent === '....') {
+        if (element.textContent === '....') {
             element.textContent = '';
         }
-    }, 300)
+    }, 300);
 }
 
 function typeText(element, text) {
-    let index = 0;
+    let index = 0
 
     let interval = setInterval(() => {
         if (index < text.length) {
             element.innerHTML += text.charAt(index)
-            index++;
+            index++
         } else {
-            clearInterval(interval);
+            clearInterval(interval)
         }
     }, 20)
 }
@@ -63,30 +63,30 @@ function chatStripe(isAi, value, uniqueId) {
 }
 
 const handleSubmit = async (e) => {
-    e.preventDefault();
+    e.preventDefault()
 
-    const data = new FormData(form);
+    const data = new FormData(form)
 
     // user's chatstripe
-    chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+    chatContainer.innerHTML += chatStripe(false, data.get('prompt'))
 
     // to clear the textarea input 
-    form.reset();
+    form.reset()
 
     // bot's chatstripe
     const uniqueId = generateUniqueId()
-    chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
+    chatContainer.innerHTML += chatStripe(true, " ", uniqueId)
 
     // to focus scroll to the bottom 
     chatContainer.scrollTop = chatContainer.scrollHeight;
 
     // specific message div 
-    const messageDiv = document.getElementById(uniqueId);
+    const messageDiv = document.getElementById(uniqueId)
 
     // messageDiv.innerHTML = "..."
-    loader(messageDiv);
+    loader(messageDiv)
 
-    const response = await fetch('https://cobai.onrender.com', {
+    const response = await fetch('https://cobai.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -97,24 +97,24 @@ const handleSubmit = async (e) => {
     })
 
     clearInterval(loadInterval)
-    messageDiv.innerHTML = '';
+    messageDiv.innerHTML = " "
 
     if (response.ok) {
         const data = await response.json();
-        const parsedData = data.bot.trim(); // trims any trailing spaces/'\n' 
+        const parsedData = data.bot.trim() // trims any trailing spaces/'\n' 
 
-        typeText(messageDiv, parsedData);
+        typeText(messageDiv, parsedData)
     } else {
         const err = await response.text()
 
-        messageDiv.innerHTML = "Something went wrong";
-        alert(err);
+        messageDiv.innerHTML = "Something went wrong"
+        alert(err)
     }
 }
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener('submit', handleSubmit)
 form.addEventListener('keyup', (e) => {
     if (e.keyCode === 13) {
-        handleSubmit(e);
+        handleSubmit(e)
     }
 })
